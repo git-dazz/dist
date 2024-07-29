866186462925b4e91de81f694d9a9bbebdfddb44
git-dazz <dazzler2022@qq.com>
2024-07-29 23:44:11 +0800

    Update app-lib.ts

--- deno/app-lib.ts
+++ deno/app-lib.ts
@@ -5,3 +5,53 @@ export async function rcfg(name = "cfg.toml", type = "toml") {
     let txt = await rt(name);
     return toml.parse(txt);
 }
+
+export async function getOnlineConfig(url, config) {
+  let replace = config?.replace;
+  delete config.replace
+  const textResponse = await fetch(url, config || {});
+  let textData = await textResponse.text();
+  if (replace) {
+    textData = replace(textData);
+  }
+  return textData;
+}
+
+
+function bufferToString(buffer) {
+  return new TextDecoder().decode(buffer);
+}
+
+export async function runCmd(exe, args) {
+  const command = new Deno.Command(exe, {
+    args: args,
+    cwd: Deno.cwd(),
+    stdin: "null",
+  });
+  let result = {};
+  try {
+    let { code, stdout, stderr } = await command.output();
+    result = {
+      code,
+      stdout: bufferToString(stdout),
+      stderr: { message: bufferToString(stderr) }
+    }
+
+  } catch (err) {
+    result = {
+      code: 1,
+      stdout: "",
+      stderr: {
+        name: err.name,
+        code: err.code,
+        message: err.message,
+        stack: err.stack,
+      },
+    };
+  }
+
+  return {
+    sh: exe + " " + (args || []).join(" "),
+    ...result,
+  };
+}
