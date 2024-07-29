767d78147b1db108922426bf9dfebbcc9e398f91
git-dazz <dazzler2022@qq.com>
2024-07-29 22:25:17 +0800

    Create fs.ts

---
+++ deno/fs.ts
@@ -0,0 +1,36 @@
+// read text
+async function rt(file, opt = {}) {
+    let text = null;
+    try {
+        text = await Deno.readTextFile(file);
+    } catch (err) {
+        if (opt?.throw) {
+            throw new Error(file, err);
+        }
+        if (opt.err) {
+            console.error(`read [${file}] error;`, err);
+        }
+    }
+    finally {
+        return text;
+    }
+
+}
+
+// write text
+async function wt(file, data, opt) {
+    try {
+        await Deno.writeTextFile(file, data, { append: !!opt?.append });
+    } catch (error) {
+        if (opt?.throw) {
+            throw new Error(file, err);
+        }
+        if (opt.err) {
+            console.error(`write [${file}] error;`, err);
+        }
+    }
+    finally {
+
+    }
+
+}
