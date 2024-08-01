75d2e19988820af0a06790c9f760de1f6e03c6ae
git-dazz <dazzler2022@qq.com>
2024-08-01 18:49:23 +0800

    Update app-lib.ts

--- deno/app-lib.ts
+++ deno/app-lib.ts
@@ -1,9 +1,39 @@
 import { rt } from "./fs.ts"
 import { toml } from "./ecd.ts"
 
+import { Edge } from 'npm:edge.js'
+
+export function random(len, chars) {
+    let str = '';
+    for (let i = 0; i < len; i++) {
+        let x = Math.floor(Math.random() * chars.length)
+        str += chars.charAt(x);
+    }
+    return str;
+};
+
+export function randomNum(len = 10) {
+    return random(len, "0123456789");
+}
+export function randomString(len = 10) {
+    return random(len, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+}
+
+export async function render(template, data) {
+    const edge = Edge.create()
+    edge.global('rdN', randomNum);
+    edge.global('rdS', randomString);
+    const txt = await edge.renderRaw(template, data);
+    return txt
+}
+
+
 export async function rcfg(name = "cfg.toml", type = "toml") {
-    let txt = await rt(name);
-    return toml.parse(txt);
+    let txt = await rt(name) || "";
+    const env = Deno.env.toObject();
+    let txt2 = await render(txt, { env, evn: env, username: 'virk' })
+    if (!txt2) return {};
+    return toml.parse(txt2);
 }
 
 export async function getOnlineConfig(url, config) {
